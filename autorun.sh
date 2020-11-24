2381fbc61b6895e69bf1a6f60e065dfd2e909fff
Marcel Arpogaus <marcel@arpogaus.de>
2020-11-24 10:20:41 +0100

    MODIFIED: start XDG autostart programms using dex

--- autorun.sh
+++ autorun.sh
@@ -7,40 +7,20 @@ function run {
    fi
 }
 
-#run xbindkeys
+# This implements the XDG autostart specification
+if ! (xrdb -query | grep -q "^awesome\.started:\strue$"); then
+	xrdb -merge <<< "awesome.started:true"
+	dex -e Awesome -a
+fi;
 
-## run (only once) processes which spawn with different name
 if (command -v gnome-keyring-daemon && ! pgrep gnome-keyring-d); then
     gnome-keyring-daemon --daemonize --login &
-fi
-if (command -v start-pulseaudio-x11 && ! pgrep pulseaudio); then
-    start-pulseaudio-x11 &
 fi
 if (command -v /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 && ! pgrep polkit-mate-aut) ; then
     /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 fi
-if (command -v  xfce4-power-manager && ! pgrep xfce4-power-man) ; then
-    xfce4-power-manager &
-fi
 
 run xfsettingsd
-run nm-applet
-run light-locker
-run xcape -e 'Super_L=Super_L|Control_L|Escape'
 run thunar --daemon
-run pa-applet
-run pamac-tray
-
-## The following are not included in minimal edition by default
-## but autorun.sh will pick them up if you install them
-
-if (command -v system-config-printer-applet && ! pgrep applet.py ); then
-  system-config-printer-applet &
-fi
-
-#run picom --experimental-backends
-run blueman-applet
-run msm_notifier
-run nextcloud
 run fusuma -d
-run redshift-gtk
+run picom --experimental-backends
