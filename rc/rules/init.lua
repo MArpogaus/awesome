18d7f1af4dac831240f574425a908556e0528af7
Marcel Arpogaus <marcel@arpogaus.de>
2021-01-26 16:58:28 +0100

    removes ayu theme; updates file headers

--- rc/rules/init.lua
+++ rc/rules/init.lua
@@ -1,30 +1,13 @@
---------------------------------------------------------------------------------
--- @File:   rules.lua
--- @Author: marcel
--- @Date:   2019-12-03 13:53:32
+-- [ author ] -*- time-stamp-pattern: "@Changed[\s]?:[\s]+%%$"; -*- ------------
+-- @File   : init.lua
+-- @Author : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 --
--- @Last Modified by: Marcel Arpogaus
--- @Last Modified at: 2020-10-19 17:04:27
+-- @Created: 2021-01-26 16:53:28 (Marcel Arpogaus)
+-- @Changed: 2021-01-20 08:37:53 (Marcel Arpogaus)
 -- [ description ] -------------------------------------------------------------
 -- ...
 -- [ license ] -----------------------------------------------------------------
--- MIT License
--- Copyright (c) 2020 Marcel Arpogaus
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
+-- ...
 --------------------------------------------------------------------------------
 -- [ required modules ] --------------------------------------------------------
 local capi = {screen = screen}
--- rc/widgets/weather.lua
+++ rc/widgets/weather.lua
@@ -1,30 +1,13 @@
---------------------------------------------------------------------------------
--- @File:   weather.lua
--- @Author: Marcel Arpogaus
--- @Date:   2019-06-16 10:35:55
+-- [ author ] -*- time-stamp-pattern: "@Changed[\s]?:[\s]+%%$"; -*- ------------
+-- @File   : weather.lua
+-- @Author : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 --
--- @Last Modified by: Marcel Arpogaus
--- @Last Modified at: 2020-11-27 13:23:06
+-- @Created: 2021-01-26 16:56:17 (Marcel Arpogaus)
+-- @Changed: 2021-01-20 08:37:53 (Marcel Arpogaus)
 -- [ description ] -------------------------------------------------------------
--- weather widgets
+-- ...
 -- [ license ] -----------------------------------------------------------------
--- MIT License
--- Copyright (c) 2020 Marcel Arpogaus
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
+-- ...
 --------------------------------------------------------------------------------
 -- [ required modules ] --------------------------------------------------------
 local math = math
