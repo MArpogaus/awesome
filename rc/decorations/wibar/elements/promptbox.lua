75d6930e440bedf44522029643304d4c6ae216fa
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-08-12 10:38:25 +0200

    adds promptbox to screen

--- rc/decorations/wibar/elements/promptbox.lua
+++ rc/decorations/wibar/elements/promptbox.lua
@@ -3,7 +3,7 @@
 -- @Author : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 --
 -- @Created: 2021-08-08 15:42:38 (Marcel Arpogaus)
--- @Changed: 2021-08-10 08:36:07 (Marcel Arpogaus)
+-- @Changed: 2021-08-12 10:00:00 (Marcel Arpogaus)
 -- [ description ] -------------------------------------------------------------
 -- ...
 -- [ license ] -----------------------------------------------------------------
@@ -18,14 +18,14 @@ local abstract_element = require('rc.decorations.abstract_element')
 local module = {}
 
 -- [ module functions ] --------------------------------------------------------
-module.init = function(_)
+module.init = function(s)
     return abstract_element.new {
-        register_fn = function(s)
+        register_fn = function(_)
             -- Create a promptbox for each screen
             s.promptbox = awful.widget.prompt()
             return s.promptbox
         end,
-        unregister_fn = function(s) s.promptbox = nil end
+        unregister_fn = function(_) s.promptbox = nil end
     }
 end
 
