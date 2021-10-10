138e7ec07f2cfae4e16d1780b35614dd3771d911
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2021-10-10 18:54:12 +0200

    removes cruft from utils module

--- rc/key_bindings/extra/actions.lua
+++ rc/key_bindings/extra/actions.lua
@@ -3,7 +3,7 @@
 -- @Author : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 --
 -- @Created: 2021-01-27 11:14:55 (Marcel Arpogaus)
--- @Changed: 2021-10-09 11:38:39 (Marcel Arpogaus)
+-- @Changed: 2021-10-10 18:51:20 (Marcel Arpogaus)
 -- [ description ] -------------------------------------------------------------
 -- ...
 -- [ license ] -----------------------------------------------------------------
@@ -23,6 +23,9 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 -- [ required modules ] --------------------------------------------------------
+-- grab environment
+local capi = {screen = screen}
+
 -- Standard awesome library
 local awful = require('awful')
 
@@ -41,6 +44,100 @@ module.defaults = {
     lock_command = 'light-locker-command -l'
 }
 
+-- [ local functions ] ---------------------------------------------------------
+-- Delete the current tag
+local function delete_tag()
+    local t = awful.screen.focused().selected_tag
+    if not t then return end
+    t:delete()
+end
+
+-- Create a new tag at the end of the list
+local function add_tag()
+    awful.prompt.run {
+        prompt = 'New tag name: ',
+        textbox = awful.screen.focused().promptbox.widget,
+        exe_callback = function(new_name)
+            if not new_name or #new_name == 0 then return end
+
+            awful.tag.add(new_name, {
+                screen = awful.screen.focused(),
+                layout = awful.layout.suit.floating
+            }):view_only()
+        end
+    }
+end
+
+-- Rename the current tag
+local function rename_tag()
+    awful.prompt.run {
+        prompt = 'Rename tag: ',
+        textbox = awful.screen.focused().promptbox.widget,
+        exe_callback = function(new_name)
+            if not new_name or #new_name == 0 then return end
+
+            local t = awful.screen.focused().selected_tag
+            if t then t.name = new_name end
+        end
+    }
+end
+
+-- taken from: https://github.com/lcpz/lain/blob/master/util/init.lua
+-- Move current tag
+-- pos in {-1, 1} <-> {previous, next} tag position
+local function move_tag(pos)
+    local tag = awful.screen.focused().selected_tag
+    if tonumber(pos) <= -1 then
+        awful.tag.move(tag.index - 1, tag)
+    else
+        awful.tag.move(tag.index + 1, tag)
+    end
+end
+
+-- taken from: https://github.com/lcpz/lain/blob/master/util/init.lua
+-- On the fly useless gaps change
+local function gaps_resize(amount, s, t)
+    local scr = s or awful.screen.focused()
+    local tag = t or scr.selected_tag
+    tag.gap = tag.gap + tonumber(amount)
+    awful.layout.arrange(scr)
+end
+
+-- taken from: https://github.com/Elv13/tyrannical/blob/master/shortcut.lua
+local function fork_tag()
+    local s = awful.screen.focused()
+    local t = s.selected_tag
+    if not t then return end
+
+    local clients = t:clients()
+    local t2 = awful.tag.add(t.name, awful.tag.getdata(t))
+
+    t2:clients(clients)
+    t2:view_only()
+end
+
+-- manage widgets
+local function update_widgets()
+    for s in capi.screen do s.update_decorations() end
+end
+local function toggle_decorations()
+    local s = awful.screen.focused()
+    s.toggle_decorations()
+end
+local function toggle_widgets()
+    local s = awful.screen.focused()
+    s.toggle_decorations_widgets()
+end
+
+-- change dpi
+local function inc_dpi(inc)
+    for s in capi.screen do
+        s.dpi = s.dpi + inc
+        utils.xconf_property_set('/Xft/DPI', math.floor(s.dpi))
+    end
+end
+local function dec_dpi(dec) module.inc_dpi(-dec) end
+
 -- [ module functions ] --------------------------------------------------------
 module.init = function(_, config)
     config = utils.deep_merge(module.defaults, config)
@@ -51,15 +148,11 @@ module.init = function(_, config)
                 awful.spawn(config.lock_command)
             end,
             ['Mac OSX like \'Exposé\' view'] = revelation.expose,
-            ['toggle decorations'] = utils.toggle_decorations
+            ['toggle decorations'] = toggle_decorations
         },
         client = {
-            ['decrement useless gaps'] = function()
-                utils.gaps_resize(-2)
-            end,
-            ['increment useless gaps'] = function()
-                utils.gaps_resize(2)
-            end
+            ['decrement useless gaps'] = function() gaps_resize(-2) end,
+            ['increment useless gaps'] = function() gaps_resize(2) end
         },
         launcher = {
             ['launch Browser'] = function()
@@ -78,21 +171,21 @@ module.init = function(_, config)
             end
         },
         tag = {
-            ['add new tag'] = utils.add_tag,
-            ['delete tag'] = utils.delete_tag,
-            ['fork tag'] = utils.fork_tag,
+            ['add new tag'] = add_tag,
+            ['delete tag'] = delete_tag,
+            ['fork tag'] = fork_tag,
             ['go back'] = awful.tag.history.restore,
-            ['move tag to the left'] = function() utils.move_tag(-1) end,
-            ['move tag to the right'] = function() utils.move_tag(1) end,
-            ['rename tag'] = utils.rename_tag
+            ['move tag to the left'] = function() move_tag(-1) end,
+            ['move tag to the right'] = function() move_tag(1) end,
+            ['rename tag'] = rename_tag
         },
         theme = {
-            ['decrease dpi'] = function() utils.dec_dpi(10) end,
-            ['increase dpi'] = function() utils.inc_dpi(10) end
+            ['decrease dpi'] = function() dec_dpi(10) end,
+            ['increase dpi'] = function() inc_dpi(10) end
         },
         widgets = {
-            ['toggle widgets'] = utils.toggle_widgets,
-            ['update widgets'] = utils.update_widgets
+            ['toggle widgets'] = toggle_widgets,
+            ['update widgets'] = update_widgets
         }
     }
 
--- rc/utils.lua
+++ rc/utils.lua
@@ -3,7 +3,7 @@
 -- @Author : Marcel Arpogaus <marcel dot arpogaus at gmail dot com>
 --
 -- @Created: 2021-01-26 16:54:31 (Marcel Arpogaus)
--- @Changed: 2021-10-09 11:39:57 (Marcel Arpogaus)
+-- @Changed: 2021-10-10 18:53:13 (Marcel Arpogaus)
 -- [ description ] -------------------------------------------------------------
 -- ...
 -- [ license ] -----------------------------------------------------------------
@@ -23,11 +23,8 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------------------
 -- [ required modules ] --------------------------------------------------------
-local capi = {screen = screen, client = client, awesome = awesome}
-
 -- Standard awesome library
 local awful = require('awful')
-local beautiful = require('beautiful')
 local gears = require('gears')
 local gfs = require('gears.filesystem')
 
@@ -37,57 +34,16 @@ local cairo = lgi.cairo
 -- [ local variables ] ---------------------------------------------------------
 local module = {}
 
--- [ local functions ] ---------------------------------------------------------
--- ref.: https://stackoverflow.com/questions/62286322/grouping-windows-in-the-tasklist
-local function client_label(c)
-    local active_theme = beautiful.get()
-    local sticky = active_theme.tasklist_sticky or '▪'
-    local ontop = active_theme.tasklist_ontop or '⌃'
-    local above = active_theme.tasklist_above or '▴'
-    local below = active_theme.tasklist_below or '▾'
-    local floating = active_theme.tasklist_floating or '✈'
-    local minimized = active_theme.tasklist_maximized or '-'
-    local maximized = active_theme.tasklist_maximized or '+'
-    local maximized_horizontal = active_theme.tasklist_maximized_horizontal or
-                                     '⬌'
-    local maximized_vertical = active_theme.tasklist_maximized_vertical or
-                                   '⬍'
-
-    local name = c.name
-    if c.sticky then name = sticky .. name end
-
-    if c.ontop then
-        name = ontop .. name
-    elseif c.above then
-        name = above .. name
-    elseif c.below then
-        name = below .. name
-    end
-
-    if c.minimized then name = minimized .. name end
-    if c.maximized then
-        name = maximized .. name
-    else
-        if c.maximized_horizontal then
-            name = maximized_horizontal .. name
-        end
-        if c.maximized_vertical then name = maximized_vertical .. name end
-        if c.floating then name = floating .. name end
-    end
-
-    return name
-end
-local function set_xconf(property, value, sleep)
-    local xconf = string.format(
-        'xfconf-query -c xsettings --property %s --set \'%s\'', property, value)
-    if sleep then xconf = string.format('sleep %.1f && %s', sleep, xconf) end
-    awful.spawn.with_shell(xconf)
-end
-
 -- [ module functions ] --------------------------------------------------------
 function module.xrdb_set_value(key, value)
     awful.spawn
         .with_shell(string.format('xrdb -merge <<< "%s:%s"', key, value))
+end
+function module.xconf_property_set(property, value, sleep)
+    local xconf = string.format(
+        'xfconf-query -c xsettings --property %s --set \'%s\'', property, value)
+    if sleep then xconf = string.format('sleep %.1f && %s', sleep, xconf) end
+    awful.spawn.with_shell(xconf)
 end
 
 -- Load configuration file
@@ -99,7 +55,6 @@ function module.load_config(config_file)
     end
     return config
 end
-function module.sleep(n) os.execute('sleep ' .. tonumber(n)) end
 
 -- deep merge two tables
 function module.deep_merge(t1, t2, max_level)
@@ -150,6 +105,7 @@ function module.value_with_cfg(t, sorted)
     end
     return iter
 end
+
 -- Helper functions for modifying hex colors -----------------------------------
 function module.darker(color, ratio)
     local pattern = gears.color(color)
@@ -201,198 +157,7 @@ function module.set_alpha(color, alpha)
     end
 end
 
--- Delete the current tag
-function module.delete_tag()
-    local t = awful.screen.focused().selected_tag
-    if not t then return end
-    t:delete()
-end
-
--- Create a new tag at the end of the list
-function module.add_tag()
-    awful.prompt.run {
-        prompt = 'New tag name: ',
-        textbox = awful.screen.focused().promptbox.widget,
-        exe_callback = function(new_name)
-            if not new_name or #new_name == 0 then return end
-
-            awful.tag.add(new_name, {
-                screen = awful.screen.focused(),
-                layout = awful.layout.suit.floating
-            }):view_only()
-        end
-    }
-end
-
--- Rename the current tag
-function module.rename_tag()
-    awful.prompt.run {
-        prompt = 'Rename tag: ',
-        textbox = awful.screen.focused().promptbox.widget,
-        exe_callback = function(new_name)
-            if not new_name or #new_name == 0 then return end
-
-            local t = awful.screen.focused().selected_tag
-            if t then t.name = new_name end
-        end
-    }
-end
-
--- taken from: https://github.com/lcpz/lain/blob/master/util/init.lua
--- Move current tag
--- pos in {-1, 1} <-> {previous, next} tag position
-function module.move_tag(pos)
-    local tag = awful.screen.focused().selected_tag
-    if tonumber(pos) <= -1 then
-        awful.tag.move(tag.index - 1, tag)
-    else
-        awful.tag.move(tag.index + 1, tag)
-    end
-end
-
--- taken from: https://github.com/lcpz/lain/blob/master/util/init.lua
--- On the fly useless gaps change
-function module.gaps_resize(amount, s, t)
-    local scr = s or awful.screen.focused()
-    local tag = t or scr.selected_tag
-    tag.gap = tag.gap + tonumber(amount)
-    awful.layout.arrange(scr)
-end
-
--- taken from: https://github.com/Elv13/tyrannical/blob/master/shortcut.lua
-function module.fork_tag()
-    local s = awful.screen.focused()
-    local t = s.selected_tag
-    if not t then return end
-
-    local clients = t:clients()
-    local t2 = awful.tag.add(t.name, awful.tag.getdata(t))
-
-    t2:clients(clients)
-    t2:view_only()
-end
-function module.get_screen(i)
-    local sc = capi.screen.count()
-    if i > sc then i = 1 end
-
-    return capi.screen[i]
-end
-function module.move_to_screen(c)
-    if not c then return end
-
-    local s = c.screen.index + 1
-
-    local s1 = awful.screen.focused()
-    local s2 = module.get_screen(s)
-
-    if s1 == s2 then return end
-
-    local t1 = s1.selected_tag
-
-    local t2 = awful.tag.find_by_name(s2, t1.name)
-    if t2 == nil then
-        local td = awful.tag.getdata(t1)
-        t2 = awful.tag.add(t1.name, gears.table.clone(td), {screen = s})
-        td.instances[s] = t2
-    end
-
-    c:move_to_screen()
-    c:move_to_tag(t2)
-    t2:view_only()
-end
-function module.client_menu_toggle_fn()
-    local instance = nil
-    return function()
-        if instance and instance.wibox.visible then
-            instance:hide()
-            instance = nil
-        else
-            instance = awful.menu.clients({theme = {width = 1000}})
-        end
-    end
-end
-
-function module.client_stack_toggle_fn()
-    local cl_menu
-    return function(c)
-        if cl_menu then
-            cl_menu:hide()
-            cl_menu = nil
-        else
-            local client_num = 0
-            local client_list = {}
-            for i, cl in ipairs(capi.client.get()) do
-                if cl.class == c.class then
-                    client_num = client_num + 1
-                    client_list[i] = {
-                        client_label(cl),
-                        function()
-                            capi.client.focus = cl
-                            cl:tags()[1]:view_only()
-                            cl:raise()
-                        end,
-                        cl.icon
-                    }
-                end
-            end
-
-            if client_num > 1 then
-                cl_menu = awful.menu({
-                    items = client_list,
-                    theme = {width = 1000}
-                })
-                cl_menu:show()
-            else
-                capi.client.focus = c
-                c:tags()[1]:view_only()
-                c:raise()
-            end
-        end
-    end
-end
-
-function module.application_switcher()
-    awful.menu.menu_keys.down = {'Tab', 'Down'}
-    awful.menu.client_list {
-        theme = {width = 1000, height = 64},
-        item_args = {keygrabber = true, coords = {x = 0, y = 0}},
-        -- TODO: why not working?
-        filter = function(c)
-            return awful.widget.tasklist.filter.currenttags(c, c.screen)
-        end
-    }
-end
-
--- change dpi
-function module.inc_dpi(inc)
-    for s in capi.screen do
-        s.dpi = s.dpi + inc
-        set_xconf('/Xft/DPI', math.floor(s.dpi))
-    end
-end
-function module.dec_dpi(dec) module.inc_dpi(-dec) end
-
--- https://github.com/awesomeWM/awesome/blob/7a8fa9d27a7907ab81e60274c925ba65d10015aa/lib/awful/screen/dpi.lua#L228
-function module.get_xft_dpi()
-    local xft_dpi = tonumber(capi.awesome.xrdb_get_value('', 'Xft.dpi')) or
-                        false
-
-    return xft_dpi
-end
-
--- manage widgets
-function module.update_widgets()
-    for s in capi.screen do s.update_decorations() end
-end
-function module.toggle_decorations()
-    local s = awful.screen.focused()
-    s.toggle_decorations()
-end
-function module.toggle_widgets()
-    local s = awful.screen.focused()
-    s.toggle_decorations_widgets()
-end
-
+-- hot reload theme
 function module.update_theme()
     -- rc modules
     local assets = require('rc.assets')
